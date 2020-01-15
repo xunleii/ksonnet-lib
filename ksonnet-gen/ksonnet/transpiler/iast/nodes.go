c905ca4eade7191f86d2e17ff26a94539b1c26c4
Alexandre Nicolaie <alexandre.nicolaie@gmail.com>
2020-01-15 23:33:24 +0100

    fixup! refact: simplify i(a)st package

--- ksonnet-gen/ksonnet/transpiler/iast/nodes.go
+++ ksonnet-gen/ksonnet/transpiler/iast/nodes.go
@@ -4,9 +4,8 @@ package iast
 
 import (
 	"fmt"
+	"path"
 	"regexp"
-
-	"github.com/ksonnet/ksonnet-lib/ksonnet-gen/ksonnet/ist"
 )
 
 // WARN: the 8 first bits must be reserved for this package
@@ -21,6 +20,8 @@ const (
 type (
 	// All node types implement this interface.
 	Node interface {
+		fmt.Stringer
+
 		// Name returns the node name
 		Name() string
 		// Description returns the API description of the node. It is used
@@ -55,6 +56,7 @@ type (
 	Reference struct {
 		NodeBase
 		ReferenceTo string
+		link        *APINode
 	}
 
 	// Scalar represents all component that are not Object nor Array.
@@ -79,23 +81,19 @@ func (n NodeBase) Parent() Node            { return n.parent }
 func (n *NodeBase) Orphanize()             { n.parent = nil }
 func (n *NodeBase) AddTag(tag NodeTag)     { n.tags |= tag }
 func (n NodeBase) HasTag(tag NodeTag) bool { return (n.tags & tag) == tag }
-
-type (
-	// APINode is a wrapper extending the Node type in order to add some information
-	// about the API Object, like its kind or group.
-	APINode struct {
-		Definition APIDefinition
-		Node
+func (n NodeBase) String() string {
+	if n.parent == nil {
+		return ""
 	}
+	return path.Join(n.parent.String(), n.name)
+}
 
-	// APIDefinition contains elements common with all API Object
-	// (like Deployment, Statefulset, Node, etc...).
-	APIDefinition struct {
-		Group   string
-		Version string
-		Kind    string
-	}
-)
+// LinkTo adds the referenced api pointer to the current instances, in order
+// to easily access after tree resolution.
+func (r *Reference) LinkTo(api *APINode) { r.link = api }
+
+// LinkedTo returns the referenced API node pointer.
+func (r Reference) LinkedTo() *APINode { return r.link }
 
 var (
 	rxApiValues = regexp.MustCompile(`(?:(?P<group>\w+)\.(?P<version>v\d+(?:(?:alpha|beta)\d+)?)\.(?P<kind>\w+)$)|(?:(?P<group_spe>\w+)\.(?P<kind_spe>\w+)$)`)
@@ -110,23 +108,47 @@ const (
 	rxIdKindSpe
 )
 
+type (
+	// APINode is a wrapper extending the Node type in order to add some information
+	// about the API Object, like its kind or group.
+	APINode struct {
+		Definition APIDefinition
+		Node       Node
+		tags       NodeTag
+	}
+
+	// APIDefinition contains elements common with all API Object
+	// (like Deployment, Statefulset, Node, etc...).
+	APIDefinition struct {
+		Group   string
+		Version string
+		Kind    string
+	}
+)
+
 // APIDefinitionFromAPIName creates an APIDefinition based on the API fullname.
-func APIDefinitionFromAPIName(fullname string) (ist.APIDefinition, error) {
+func APIDefinitionFromAPIName(fullname string) (APIDefinition, error) {
 	submatch := rxApiValues.FindStringSubmatch(fullname)
 	if len(submatch) == 0 {
-		return ist.APIDefinition{}, fmt.Errorf("unknown API fullname '%s': must match the regex '%s'", fullname, rxApiValues)
+		return APIDefinition{}, fmt.Errorf("invalid API object '%s': must match the regex '%s'", fullname, rxApiValues)
 	}
 
 	if submatch[rxIdGroup] != "" {
-		return ist.APIDefinition{
+		return APIDefinition{
 			Group:   submatch[rxIdGroup],
 			Version: submatch[rxIdVersion],
 			Kind:    submatch[rxIdKind],
 		}, nil
 	} else {
-		return ist.APIDefinition{
+		return APIDefinition{
 			Group: submatch[rxIdGroupSpe],
 			Kind:  submatch[rxIdKindSpe],
 		}, nil
 	}
 }
+
+// AddTag adds a tag to the current node.
+func (n *APINode) AddTag(tag NodeTag) { n.tags |= tag }
+
+// HasTag returns true if the given current node as the given tag.
+func (n APINode) HasTag(tag NodeTag) bool { return (n.tags & tag) == tag }
