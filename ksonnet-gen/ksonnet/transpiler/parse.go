51859d80d3307d02f7b6de962789e471569e33b9
Alexandre Nicolaie <alexandre.nicolaie@gmail.com>
2020-01-17 22:02:15 +0100

    refact: rewrite factory into parsing functions

---
+++ ksonnet-gen/ksonnet/transpiler/parse.go
@@ -0,0 +1,142 @@
+package transpiler
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-openapi/spec"
+
+	"github.com/ksonnet/ksonnet-lib/ksonnet-gen/ksonnet/transpiler/iast"
+)
+
+// parseAPISpec extracts all API specification of the Swagger object and returns
+// a list of iast.APINode, which can be translated into a Jsonnet ast.
+func parseAPISpec(spec *spec.Swagger, config Config) (map[string]*iast.APINode, error) {
+	if spec == nil {
+		return nil, fmt.Errorf("spec.Swagger provided cannot be nil")
+	}
+
+	var apis = map[string]*iast.APINode{}
+	for api, schema := range spec.SwaggerProps.Definitions {
+		definition, err := iast.APIDefinitionFromAPIName(api)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse API object '%s': %w", api, err)
+		}
+
+		node, err := parseSchema("", &schema, nil, config)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse API object '%s': %w", api, err)
+		}
+
+		apis[api] = &iast.APINode{Definition: definition, Description: schema.Description, Node: node}
+	}
+
+	return apis, nil
+}
+
+// parseSchema converts an API schema to an iast.Node, according
+// to the schema type.
+func parseSchema(name string, schema *spec.Schema, parent iast.Node, config Config) (iast.Node, error) {
+	if schema.Type == nil {
+		// schema.Type is nil and schema.Ref.HasFragmentOnly == true is valid
+		// when schema is a reference
+		if schema.Ref.HasFragmentOnly == true {
+			return parseReferenceSchema(name, schema, parent)
+		}
+
+		// in this case, we manage the unknown schema as scalar object
+		// (generally JSON properties)
+		return parseScalarSchema(name, schema, parent)
+	}
+
+	if len(schema.Type) == 0 {
+		return nil, fmt.Errorf("failed to build node '%s': no schema.Type provided", name)
+	}
+
+	switch schema.Type[0] {
+	case "object":
+		return parseObjectSchema(name, schema, parent, config)
+	case "array":
+		return parseArraySchema(name, schema, parent, config)
+	case "boolean", "integer", "number", "string":
+		return parseScalarSchema(name, schema, parent)
+	default:
+		return nil, fmt.Errorf("failed to build node '%s': unknown schema.Type '%s'", name, schema.Type[0])
+	}
+}
+
+// parseObjectSchema creates an iast.Object node from a schema object.
+func parseObjectSchema(name string, schema *spec.Schema, parent iast.Node, config Config) (iast.Node, error) {
+	// object without properties must be managed as a scalar type
+	if len(schema.Properties) == 0 {
+		return parseScalarSchema(name, schema, parent)
+	}
+
+	node := &iast.Object{
+		NodeBase: iast.NewNodeBase(name, schema.Description, parent),
+		Fields:   map[string]iast.Node{},
+	}
+
+	for key, prop := range schema.Properties {
+		// ignore if the key must be ignored
+		if _, ignored := config.ignoredFieldsMap[key]; ignored {
+			continue
+		}
+
+		field, err := parseSchema(key, &prop, node, config)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create node field '%s' of %s: %w", key, name, err)
+		}
+
+		node.Fields[key] = field
+	}
+
+	return node, nil
+}
+
+// parseArraySchema creates an iast.Array node from a schema array.
+// WARN: An array must have only one item type.
+func parseArraySchema(name string, schema *spec.Schema, parent iast.Node, config Config) (iast.Node, error) {
+	if schema.Items.Len() != 1 {
+		return nil, fmt.Errorf("invalid array '%s': it must contains one and only one inner type", name)
+	}
+
+	var err error
+	innerType, err := parseSchema("", schema.Items.Schema, nil, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &iast.Array{
+		NodeBase: iast.NewNodeBase(name, schema.Description, parent),
+		ItemType: innerType,
+	}, nil
+}
+
+// parseScalarSchema creates an ist.Scalar node, representing a node
+// other than object, reference or array.
+func parseScalarSchema(name string, schema *spec.Schema, parent iast.Node) (node iast.Node, err error) {
+	return &iast.Scalar{
+		NodeBase: iast.NewNodeBase(name, schema.Description, parent),
+	}, nil
+}
+
+// parseReferenceSchema creates an ist.Ref node, a reference link to
+// another node.
+func parseReferenceSchema(name string, schema *spec.Schema, parent iast.Node) (iast.Node, error) {
+	var reference string
+	if schema.Ref.GetPointer() != nil {
+		parts := strings.Split(schema.Ref.GetPointer().String(), "/")
+		reference = parts[len(parts)-1]
+	}
+
+	// if there is no type and no reference, we manage is has scalar type
+	if reference == "" {
+		return &iast.Scalar{NodeBase: iast.NewNodeBase(name, schema.Description, parent)}, nil
+	}
+
+	return &iast.Reference{
+		NodeBase:    iast.NewNodeBase(name, schema.Description, parent),
+		ReferenceTo: reference,
+	}, nil
+}
